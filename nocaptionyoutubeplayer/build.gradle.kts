3f8ba2b00f99e5c87e4e183a0d8577c5a2bd7b5a
지운만 <manjee.official@gmail.com>
2024-01-29 14:33:50 +0900

    Add basic project

---
+++ nocaptionyoutubeplayer/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.manjee.nocaptionyoutubeplayer"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            create<MavenPublication>("release") {
+                // Applies the component for the release build variant.
+                from(components["release"])
+
+                // You can then customize attributes of the publication as shown below.
+                groupId = "com.github.manjee-official"
+                artifactId = "NoCaptionYoutubePlayer"
+                version = "0.0.1"
+            }
+            // Creates a Maven publication called “debug”.
+            create<MavenPublication>("debug") {
+                // Applies the component for the debug build variant.
+                from(components["debug"])
+
+                groupId = "com.github.manjee-official"
+                artifactId = "NoCaptionYoutubePlayer"
+                version = "0.0.1"
+            }
+        }
+    }
+}
